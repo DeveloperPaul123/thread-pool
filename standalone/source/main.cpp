03672612dfd6656f3f265ceac07d21d42c8a1460
Paul T <developer.paul.123@gmail.com>
2021-10-22 15:35:00 -0400

    Update Standalone Executable

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -9,15 +9,15 @@
 auto main(int argc, char** argv) -> int {
   cxxopts::Options options(*argv, "A program to welcome the world!");
 
-  std::string language;
-  std::string name;
+  int image_width;
+  int image_height;
 
   // clang-format off
   options.add_options()
     ("h,help", "Show help")
     ("v,version", "Print the current version number")
-    ("n,name", "Name to greet", cxxopts::value(name)->default_value("World"))
-    ("l,lang", "Language code to use", cxxopts::value(language)->default_value("en"))
+    ("w,width", "Image width", cxxopts::value(image_width)->default_value("1080"))
+    ("h,height", "Image height", cxxopts::value(image_height)->default_value("1920"))
   ;
   // clang-format on
 
@@ -29,7 +29,5 @@ auto main(int argc, char** argv) -> int {
   pool.enqueue_detach([]() { std::cout << "Hi from " << std::this_thread::get_id() << std::endl; });
   pool.enqueue_detach([]() { std::cout << "Hi from " << std::this_thread::get_id() << std::endl; });
  
-  // TODO: Wait for pool to finish
-  std::this_thread::sleep_for(std::chrono::milliseconds(3000));
   return 0;
 }
