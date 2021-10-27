1188c626f67fc49680db78f705aafc60aa43c121
Paul T <developer.paul.123@gmail.com>
2021-10-27 16:12:51 -0400

    New Unit Test
    Added test to check that return values in task futures are correct.

--- test/source/thread_pool.cpp
+++ test/source/thread_pool.cpp
@@ -3,7 +3,7 @@
 
 #include <string>
 
-TEST_CASE("ThreadPool") {
+TEST_CASE("Basic Return Types") {
   dp::thread_pool pool(2);
   // TODO
   auto future_value = pool.enqueue([](const int& value) { return value; }, 30);
@@ -14,3 +14,20 @@ TEST_CASE("ThreadPool") {
   CHECK(value1 == 30);
   CHECK(value2 == 3 - 20);
 }
+
+TEST_CASE("Ensure input params are properly passed") {
+  dp::thread_pool pool(4);
+  const auto total_tasks = 30;
+  std::vector<std::future<int>> futures;
+
+  for (auto i = 0; i < total_tasks; i++) {
+    auto task = [index = i]() { return index; };
+
+    futures.push_back(pool.enqueue(task));
+  }
+
+  for (auto j = 0; j < total_tasks; j++) {
+    CHECK(j == futures[j].get());
+  }
+}
+
