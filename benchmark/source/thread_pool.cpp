efe73867e95f3ffab72245ab386e746aaa7b6496
Paul T <developer.paul.123@gmail.com>
2022-08-19 17:04:14 -0400

    WIP: Add coroutine thread pool benchmark

--- benchmark/source/thread_pool.cpp
+++ benchmark/source/thread_pool.cpp
@@ -1,14 +1,18 @@
 #include <benchmark/benchmark.h>
 #include <thread_pool/thread_pool.h>
 
+#include <cppcoro/sync_wait.hpp>
+#include <cppcoro/when_all.hpp>
+
 #include "utilities.h"
 
-static void BM_array_multiplication(benchmark::State& state) {
+static void BM_array_multiplication_thread_pool(benchmark::State& state) {
     const auto array_size = state.range(0);
     const std::size_t multiplications_to_perform = state.range(1);
 
     // generate the data
-    const auto computations = generate_benchmark_data<int>(array_size, multiplications_to_perform);
+    const auto computations = generate_benchmark_data<int>(array_size,
+    multiplications_to_perform);
 
     // task that is run on a new thread
     auto thread_task = [](multiplication_pair<int> pair) {
@@ -28,7 +32,7 @@ static void BM_array_multiplication(benchmark::State& state) {
     }
 }
 
-BENCHMARK(BM_array_multiplication)
+BENCHMARK(BM_array_multiplication_thread_pool)
     ->Args({8, 25'000})
     ->Args({64, 5'000})
     ->Args({256, 250})
@@ -39,3 +43,34 @@ BENCHMARK(BM_array_multiplication)
     ->MeasureProcessCPUTime()
     ->UseRealTime()
     ->Name("dp::thread_pool array mult");
+
+inline cppcoro::task<> mult_task(multiplication_pair<int> pair, dp::thread_pool<>& pool) {
+    std::vector<int> result(pair.first.size());
+    co_await co_multiply_array(pair.first, pair.second, result, pool);
+}
+
+static void BM_array_multiplication_thread_pool_coroutine(benchmark::State& state) {
+    const auto array_size = state.range(0);
+    const std::size_t multiplications_to_perform = state.range(1);
+    const auto computations = generate_benchmark_data<int>(array_size, multiplications_to_perform);
+    dp::thread_pool pool{};
+    std::vector<cppcoro::task<>> tasks;
+    for (auto _ : state) {
+        for (auto mult_pair : computations) {
+            tasks.emplace_back(mult_task(mult_pair, pool));
+        }
+    }
+    cppcoro::sync_wait(cppcoro::when_all(std::move(tasks)));
+}
+
+BENCHMARK(BM_array_multiplication_thread_pool_coroutine)
+    ->Args({8, 25'000})
+    ->Args({64, 5'000})
+    ->Args({256, 250})
+    ->Args({512, 75})
+    ->Args({1024, 10})
+    ->Unit(benchmark::kMillisecond)
+    ->ReportAggregatesOnly(true)
+    ->MeasureProcessCPUTime()
+    ->UseRealTime()
+    ->Name("dp::thread_pool coroutine array mult");
