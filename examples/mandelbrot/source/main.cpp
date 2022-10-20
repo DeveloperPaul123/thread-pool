5358ecc5c985fe8bf7e2f7d5d73412209ae2ffcb
Paul T <developer.paul.123@gmail.com>
2022-10-20 12:38:15 -0400

    Add version output to mandelbrot example

--- examples/mandelbrot/source/main.cpp
+++ examples/mandelbrot/source/main.cpp
@@ -1,8 +1,10 @@
 #include <thread_pool/thread_pool.h>
+#include <thread_pool/version.h>
 
 #include <chrono>
 #include <cxxopts.hpp>
 #include <filesystem>
+#include <format>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -72,6 +74,8 @@ auto main(int argc, char **argv) -> int {
 
     try {
         const auto result = options.parse(argc, argv);
+
+        std::cout << std::format("Using dp::thread-pool version {:s}\n", THREADPOOL_VERSION);
 
         if (result.count("help")) {
             std::cout << options.help({"", "Group"}) << std::endl;
