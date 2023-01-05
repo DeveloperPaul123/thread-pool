515a1a695d233c9ab3c2b3d4a9437cf58d44fe3a
Paul T <developer.paul.123@gmail.com>
2023-01-05 09:41:21 -0500

    Improve queue unit test with barrier
    Use a barrier to synchronize threads when testing the thread safe queue

--- test/source/thread_safe_queue.cpp
+++ test/source/thread_safe_queue.cpp
@@ -1,31 +1,53 @@
 #include <doctest/doctest.h>
 #include <thread_pool/thread_safe_queue.h>
 
+#include <barrier>
 #include <future>
+#include <thread>
 
 TEST_CASE("Ensure insert and pop works with thread contention") {
-    dp::thread_safe_queue<int> queue;
+    // create a synchronization barrier to ensure our threads have started before executing code to
+    // access the queue
+    std::barrier barrier(3);
+    std::promise<int> p1, p2, p3;
+    {
+        // initialize the queue
+        dp::thread_safe_queue<int> queue;
+        std::jthread t1([&queue, &barrier, &p1] {
+            barrier.arrive_and_wait();
+            queue.push(1);
+            barrier.arrive_and_wait();
+            p1.set_value(queue.pop().value_or(-1));
+        });
+        std::jthread t2([&queue, &barrier, &p2] {
+            barrier.arrive_and_wait();
+            queue.push(2);
+            barrier.arrive_and_wait();
+            p2.set_value(queue.pop().value_or(-1));
+        });
+        std::jthread t3([&queue, &barrier, &p3] {
+            barrier.arrive_and_wait();
+            queue.push(3);
+            barrier.arrive_and_wait();
+            p3.set_value(queue.pop().value_or(-1));
+        });
+    }
 
-    auto fut1 = std::async([&queue]() { queue.push(1); });
-    auto fut2 = std::async([&queue]() { queue.push(2); });
-    auto fut3 = std::async([&queue]() { queue.push(3); });
+    // get the popped values
+    auto fut1 = p1.get_future();
+    auto fut2 = p2.get_future();
+    auto fut3 = p3.get_future();
+    auto res1 = fut1.get();
+    auto res2 = fut2.get();
+    auto res3 = fut3.get();
 
-    auto fut4 = std::async([&queue]() { return queue.pop(); });
-    auto fut5 = std::async([&queue]() { return queue.pop(); });
-    auto fut6 = std::async([&queue]() { return queue.pop(); });
-
-    fut1.get(), fut2.get(), fut3.get();
-
-    auto res4 = fut4.get();
-    auto res5 = fut5.get();
-    auto res6 = fut6.get();
-
-    REQUIRE(res4.has_value());
-    REQUIRE(res5.has_value());
-    REQUIRE(res6.has_value());
+    // ensure that we got a value
+    CHECK_NE(res1, -1);
+    CHECK_NE(res2, -1);
+    CHECK_NE(res3, -1);
 
     // we don't know what order that the values were pushed into the queue
-    CHECK_NE(res4.value(), res5.value());
-    CHECK_NE(res5.value(), res6.value());
-    CHECK_NE(res6.value(), res4.value());
+    CHECK_NE(res1, res2);
+    CHECK_NE(res2, res3);
+    CHECK_NE(res3, res1);
 }
