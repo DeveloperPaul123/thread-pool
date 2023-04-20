e15dc40bab8606c40c2ad67980c532f02ffff269
Paul T <developer.paul.123@gmail.com>
2023-04-20 09:14:53 -0400

    Update benchmark setup
    Updated benchmarks to use nanobench and compare my thread pool to other implementations instead of comparing to std::async

--- benchmark/source/main.cpp
+++ benchmark/source/main.cpp
@@ -1,25 +1,3 @@
-#include <benchmark/benchmark.h>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
-#include <array>
-
-#include "utilities.h"
-
-int main(int argc, char** argv) {
-    // quick test for matrix multiplication
-    std::array a{3, 7, 4, 9};
-    std::array b{6, 2, 5, 8};
-    std::array<int, 4> c{};
-
-    multiply_array(a, b, c);
-    assert(c[0] == 53);
-    assert(c[1] == 62);
-    assert(c[2] == 69);
-    assert(c[3] == 80);
-
-    // set up all the benchmarks and run them
-    ::benchmark::Initialize(&argc, argv);
-    if (::benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
-    ::benchmark::RunSpecifiedBenchmarks();
-    ::benchmark::Shutdown();
-    return 0;
-}
+#include <doctest/doctest.h>
---
+++ benchmark/source/matrix_multiplication.cpp
@@ -0,0 +1,134 @@
+#include <doctest/doctest.h>
+#include <nanobench.h>
+#include <thread_pool/thread_pool.h>
+
+// thread pool libraries
+#include <BS_thread_pool.hpp>
+#include <boost/asio.hpp>
+#include <concepts>
+#include <fstream>
+#include <future>
+#include <riften/thiefpool.hpp>
+
+#include "boost/asio/post.hpp"
+#include "utilities.h"
+
+template <std::integral DataType, typename FutureProvider>
+    requires std::invocable<FutureProvider, std::vector<DataType>, std::vector<DataType>>
+void run_benchmark(ankerl::nanobench::Bench* bench, const std::size_t array_size,
+                   const std::size_t multiplications_to_perform, const char* name,
+                   FutureProvider&& provider) {
+    // generate test data
+    // const auto computations = generate_benchmark_data<int>(array_size,
+    // multiplications_to_perform);
+    const std::vector<DataType> a(array_size, 2);
+    const std::vector<DataType> b(array_size, 3);
+
+    // set up vector for results
+    std::vector<std::future<void>> results;
+
+    if constexpr (!std::is_void_v<std::invoke_result_t<FutureProvider, std::vector<DataType>,
+                                                       std::vector<DataType>>>) {
+        results.reserve(multiplications_to_perform);
+    }
+
+    bench->run(name, [&]() {
+        for (std::size_t i = 0; i < multiplications_to_perform; ++i) {
+            // let std async decide on how to launch the task, either deferred or async
+            if constexpr (std::is_same_v<std::future<void>,
+                                         std::invoke_result_t<FutureProvider, std::vector<DataType>,
+                                                              std::vector<DataType>>>) {
+                results.emplace_back(provider(std::ref(a), std::ref(b)));
+
+            } else {
+                provider(std::ref(a), std::ref(b));
+            }
+        }
+
+        if (!results.empty()) {
+            // wait for futures
+            for (auto& fut : results) {
+                fut.wait();
+            }
+        }
+    });
+}
+
+TEST_CASE("matrix_multiplication") {
+    using namespace std::chrono_literals;
+
+    // task that runs on a new thread (potentially)
+    auto thread_task = [](const std::vector<int>& a, const std::vector<int>& b) {
+        std::vector<int> result(a.size());
+        multiply_array(a, b, result);
+        ankerl::nanobench::doNotOptimizeAway(result);
+    };
+
+    std::vector<std::pair<std::size_t, std::size_t>> args = {
+        {8, 100'000}, {64, 75'000}, {256, 50'000}, {512, 35'000}, {1024, 25'000}};
+
+    std::ofstream output_file(RESULTS_MARKDOWN_FILE, std::ios::out);
+
+    for (const auto& [array_size, iterations] : args) {
+        ankerl::nanobench::Bench bench;
+        bench.title(std::format("matrix multiplication {}x{}", array_size, array_size))
+            .warmup(10)
+            .relative(true)
+            .minEpochIterations(15)
+            .output(&output_file);
+        bench.timeUnit(1ms, "ms");
+
+        {
+            dp::thread_pool<std::function<void()>> pool{};
+            run_benchmark<int>(&bench, array_size, iterations, "dp::thread_pool - std::function",
+                               [&](const std::vector<int>& a, const std::vector<int>& b) -> void {
+                                   pool.enqueue_detach(thread_task, a, b);
+                               });
+        }
+
+#ifdef __cpp_lib_move_only_function
+        {
+            dp::thread_pool pool{};
+            run_benchmark<int>(&bench, array_size, iterations,
+                               "dp::thread_pool - std::move_only_function",
+                               [&](const std::vector<int>& a, const std::vector<int>& b) -> void {
+                                   pool.enqueue_detach(thread_task, a, b);
+                               });
+        }
+#endif
+        {
+            dp::thread_pool<fu2::unique_function<void()>> pool{};
+            run_benchmark<int>(&bench, array_size, iterations,
+                               "dp::thread_pool - fu2::unique_function",
+                               [&](const std::vector<int>& a, const std::vector<int>& b) -> void {
+                                   pool.enqueue_detach(thread_task, a, b);
+                               });
+        }
+
+        {
+            BS::thread_pool bs_thread_pool{std::thread::hardware_concurrency()};
+            run_benchmark<int>(&bench, array_size, iterations, "BS::thread_pool",
+                               [&](const std::vector<int>& a, const std::vector<int>& b) -> void {
+                                   bs_thread_pool.push_task(thread_task, a, b);
+                               });
+        }
+
+        {
+            riften::Thiefpool riften_thiefpool{};
+            run_benchmark<int>(&bench, array_size, iterations, "riften::Thiefpool",
+                               [&](const std::vector<int>& a, const std::vector<int>& b) -> void {
+                                   riften_thiefpool.enqueue_detach(thread_task, a, b);
+                               });
+        }
+
+        // {
+        //     boost::asio::static_thread_pool boost_thread_pool(std::thread::hardware_concurrency());
+        //
+        //     run_benchmark<int>(&bench, array_size, iterations, "boost::static_thread_pool",
+        //                        [&](const std::vector<int>& a, const std::vector<int>& b) -> void {
+        //                            boost::asio::post(boost_thread_pool,
+        //                                              [&]() { thread_task(a, b); });
+        //                        });
+        // }
+    }
+}
